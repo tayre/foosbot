c18cf921df2753597a651f73a417c74f0bc1d965
Pim Vermeyden <pim.vermeyden@vidyard.com>
2016-03-02 21:02:53 -0500

    graceful server shutdown

--- server/index.js
+++ server/index.js
@@ -15,6 +15,27 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var server = http.listen(3000, function(){
+  console.log('listening on *:80');
 });
+
+// this function is called when you want the server to die gracefully
+// i.e. wait for existing connections
+var gracefulShutdown = function() {
+  console.log("Received kill signal, shutting down gracefully.");
+  server.close(function() {
+    console.log("Closed out remaining connections.");
+    process.exit()
+  });
+
+  setTimeout(function() {
+    console.error("Could not close connections in time, forcefully shutting down.");
+    process.exit()
+  }, 2000);
+}
+
+// listen for TERM signal .e.g. kill
+process.on ('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on ('SIGINT', gracefulShutdown);
